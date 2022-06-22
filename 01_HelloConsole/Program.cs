5985d8928cc3b23efd5be3ef5df83ca7a6dd0be8
Romanov Dmitriy <d.romanov@epc-it.by>
2022-06-22 12:52:17 +0300

    add Lessos1 HelloWorld_HelloConsole

---
+++ 01_HelloConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01_HelloConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //output information to user
+            Console.WriteLine("Hello World!!");            
+        }
+    }
+}
